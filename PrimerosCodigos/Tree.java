cd780ca0b28ebf7dc9326a2cb879a81bd90dcd94
smendivelsor <142945447+smendivelsor@users.noreply.github.com>
2023-09-05 16:54:48 -0500

    Add files via upload

---
+++ PrimerosCodigos/Tree.java
@@ -0,0 +1,21 @@
+
+package tree;
+
+public class Tree {
+
+ public static void main(String args[] ) {
+     System.out.println("Hey there,I'm Henry and I'm learnig code in java!");
+     System.out.println("I'm goint to plant a tree today!” or “Ready to get my hands dirty!");
+     System.out.println("    *    ");
+     System.out.println("   ***   ");  
+     System.out.println(" ******* ");  
+     System.out.println("  *****  ");  
+     System.out.println("   ***   ");  
+     System.out.println("    *    ");  
+     System.out.println("    *    ");
+     System.out.println("    *    ");
+     System.out.println("    *    ");
+     
+    }
+    
+}
