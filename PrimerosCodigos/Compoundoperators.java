24987ed72f649edb03cd8606881dc2deda12bcde
smendivelsor <142945447+smendivelsor@users.noreply.github.com>
2023-09-05 17:30:42 -0500

    Add files via upload

---
+++ PrimerosCodigos/Compoundoperators.java
@@ -0,0 +1,23 @@
+
+package compoundoperators;
+
+public class Compoundoperators {
+
+    public static void main(String[] args) {
+        int numCookies = 17;
+        numCookies -= 3;
+        System.out.println(numCookies);
+        numCookies /= 2;
+        
+        System.out.println(numCookies);
+    }
+    
+}
+//Compound operators:
+/*
+Addition (+=)
+Subtraction (-=)
+Multiplication (*=)
+Division (/=)
+Modulo (%=)
+*/ 
