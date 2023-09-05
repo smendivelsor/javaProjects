1fcbc3fb03270846d364b25c2eeb41e4a6f380ee
smendivelsor <142945447+smendivelsor@users.noreply.github.com>
2023-09-05 17:22:34 -0500

    Add files via upload

---
+++ PrimerosCodigos/Char.java
@@ -0,0 +1,11 @@
+
+package pkgchar;
+
+public class Char {
+
+    public static void main(String[] args) {
+        char expectedGrade = 'C';
+        System.out.println(expectedGrade);
+    }
+    
+}
