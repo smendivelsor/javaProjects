eefdb1f7da8cc0c8a90d6c844a28bf5d7269c3d4
smendivelsor <142945447+smendivelsor@users.noreply.github.com>
2023-09-05 17:17:45 -0500

    Add files via upload

---
+++ PrimerosCodigos/Booleans.java
@@ -0,0 +1,12 @@
+
+package booleans;
+
+public class Booleans {
+
+    public static void main(String[] args) {
+    boolean intsCanHoldDecimals = false;
+    
+    System.out.println(intsCanHoldDecimals);
+    }
+    
+}
