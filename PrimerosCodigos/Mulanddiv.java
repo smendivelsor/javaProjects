a7d977be305e486dc73ecbb3d89ec4b0ae1241a2
smendivelsor <142945447+smendivelsor@users.noreply.github.com>
2023-09-23 22:17:58 -0500

    Add files via upload

---
+++ PrimerosCodigos/Mulanddiv.java
@@ -0,0 +1,15 @@
+package mulanddiv;
+
+public class Mulanddiv {
+
+    public static void main(String[] args) {
+        double subtotal = 30;
+        double tax = 0.0875;
+        double total = subtotal + (subtotal*tax);
+        double perPerson = total/4;
+        
+        System.out.println(total);
+        System.out.println(perPerson);
+    }
+    
+}
