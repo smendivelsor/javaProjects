a043a5dccce4117376226e93fb21d9148a96e7ab
smendivelsor <142945447+smendivelsor@users.noreply.github.com>
2023-09-23 21:59:17 -0500

    Update Tree.java

--- PrimerosCodigos/Tree.java
+++ PrimerosCodigos/Tree.java
@@ -4,7 +4,7 @@ package tree;
 public class Tree {
 
  public static void main(String args[] ) {
-     System.out.println("Hey there,I'm Henry and I'm learnig code in java!");
+     System.out.println("Hey there,I'm Santiago and I'm learnig code in java!");
      System.out.println("I'm goint to plant a tree today!” or “Ready to get my hands dirty!");
      System.out.println("    *    ");
      System.out.println("   ***   ");  
