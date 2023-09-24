c7b8585277628d792edc21c55a9b605ce5d28516
smendivelsor <142945447+smendivelsor@users.noreply.github.com>
2023-09-23 23:09:29 -0500

    Add files via upload

---
+++ PrimerosCodigos/Variables1.java
@@ -0,0 +1,16 @@
+package variables1;
+
+
+public class Variables1 {
+
+    public static void main(String[] args) {
+        
+        String name = "James Gosling";
+        int yearCreated = 1995;
+        
+        System.out.println(name);
+        System.out.println(yearCreated);
+        
+    }
+    
+}
