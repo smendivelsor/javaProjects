d505afb684c905a49dce029172ffec333cb0ceb9
smendivelsor <142945447+smendivelsor@users.noreply.github.com>
2023-09-23 22:04:37 -0500

    Add files via upload

---
+++ PrimerosCodigos/Final.java
@@ -0,0 +1,14 @@
+
+package pkgfinal;
+
+public class Final {
+
+    public static void main(String[] args) {
+       final double pi = 3.14 ;
+       
+       System.out.println(pi);
+       pi = 2*pi;
+       
+    }
+    
+}
