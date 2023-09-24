8d536648c05c2a1b7ab582cfdb84abcbee659544
smendivelsor <142945447+smendivelsor@users.noreply.github.com>
2023-09-23 22:11:40 -0500

    Add files via upload

---
+++ PrimerosCodigos/Modulo_Example.java
@@ -0,0 +1,14 @@
+package modulo_example;
+
+public class Modulo_Example {
+
+    public static void main(String[] args) {
+        
+        int students = 26;
+        int leftOut = students%3;
+        
+        System.out.println(leftOut);
+        
+    }
+    
+}
