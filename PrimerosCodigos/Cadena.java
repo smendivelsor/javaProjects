98c89fa17be4fef8deaaec0774dda4e6735a5a3d
smendivelsor <142945447+smendivelsor@users.noreply.github.com>
2023-09-23 23:04:03 -0500

    Add files via upload

---
+++ PrimerosCodigos/Cadena.java
@@ -0,0 +1,12 @@
+package cadena;
+
+
+public class Cadena {
+  
+    public static void main(String[] args) {
+        
+        String openingLyrics = "Yesterday,all my troubles seemed so far away";
+        System.out.println(openingLyrics);
+    }
+    
+}
