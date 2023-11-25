95b70bcc0e20962a422f75b62de2209a1dc9d221
smendivelsor <142945447+smendivelsor@users.noreply.github.com>
2023-11-25 01:04:41 -0500

    Update Client.java

--- Segundo_Corte/JOptionpane/Abstracta/Inhiretance/Client.java
+++ Segundo_Corte/JOptionpane/Abstracta/Inhiretance/Client.java
@@ -1,13 +1,49 @@
-/*
- * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
- */
 package inhiretance1;
 
-/**
- *
- * @author Lenovo
- */
-public class Client {
+import java.util.Date;
+
+public class Client extends Person{
+    private int idCliente;
+    private Date fechaRegsitro;
+    private boolean vip;
+    private static int contadorCliente;
     
+    public Client(Date fechaRegsitri, boolean vip, String nombre, 
+            char genero, int edad, String direccion) {
+        
+        super(nombre, genero, edad, direccion);
+        this.idCliente = ++Client.contadorCliente;
+        this.fechaRegsitro = fechaRegsitri;
+        this.vip = vip;
+    }
+    
+    public int getIdCliente() {
+        return idCliente;
+    }
+    public Date getFechaRegsitri() {
+        return fechaRegsitro;
+    }
+    public void setFechaRegsitri(Date fechaRegsitri) {
+        this.fechaRegsitro = fechaRegsitri;
+    }
+    public boolean isVip() {
+        return vip;
+    }
+    public void setVip(boolean vip) {
+        this.vip = vip;
+    }
+
+    public String obtenerDetalle() {
+        return super.obtenerDetalle() + "Es VIP: " + this.vip;
+    }
+
+    public String toString() {
+        StringBuilder sb = new StringBuilder();
+        sb.append("Cliente{idCliente=").append(this.idCliente);
+        sb.append(", fechaRegsitri=").append(this.fechaRegsitro);
+        sb.append(", vip=").append(vip);
+        sb.append(", ").append(super.toString());
+        sb.append('}');
+        return sb.toString();
+    }     
 }
