337ee123ea3c7238c090965b8503c5a0573e0f3b
smendivelsor <142945447+smendivelsor@users.noreply.github.com>
2023-11-25 01:09:18 -0500

    Create TestHerencia.java

---
+++ Segundo_Corte/JOptionpane/Abstracta/Inhiretance/TestHerencia.java
@@ -0,0 +1,58 @@
+package test;
+
+import inhiretance1.*;
+
+import java.util.Date;
+public class TestHerencia {
+    public static void main(String[] args) {
+        
+        Person persona = new Person("Alex", 'M', 25, 
+                "Cusco");
+        
+        imprimir(persona);
+        
+        Employee empleado1 = new Employee(3000,"Gutierrez");
+      ;
+        imprimir(empleado1);
+        
+        var fecha = new Date();
+        Client cliente = new Client(fecha,true,
+                "Juan",'M',25,"Santo Tomás");
+                        
+        imprimir(cliente);
+        determinarTipo(persona);
+        determinarTipo(empleado1);
+        determinarTipo(cliente);
+        
+        String miNombre = "Santiago";
+        determinarTipo(miNombre);
+        
+            
+    Person p1 = new Person("Santiago", 'M', 19, "Bogotá");
+    Person p2 = new Person("santiago", 'M', 19, "Bogotá");
+    
+    System.out.println(p1 == p2);
+    System.out.println("Hola" == "Hola");
+    System.out.println(p1.equals(p2));
+    
+    
+    }        
+    
+    public static void imprimir(Person personaje) {
+        System.out.println(personaje.obtenerDetalle());
+    }
+    
+    
+           
+    //Código instance of en java 
+    public static void determinarTipo(Object objeto){
+        if(objeto instanceof Employee){
+            System.out.println("Es de Tipo Empleado");
+        }else if (objeto instanceof Client){
+            System.out.println("Es de tipo Cliente");
+        }else if (objeto instanceof Person){
+            System.out.println("Es de Tipo Persona");
+        }else if (objeto instanceof Object){
+            System.out.println("Es de tipo Object");
+        }
+    }
