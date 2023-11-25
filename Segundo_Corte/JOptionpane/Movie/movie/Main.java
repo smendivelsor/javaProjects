91422a45fd9a2921d1fccbe002bd9a9f441163a6
smendivelsor <142945447+smendivelsor@users.noreply.github.com>
2023-11-24 23:59:55 -0500

    Add files via upload

---
+++ Segundo_Corte/JOptionpane/Movie/movie/Main.java
@@ -0,0 +1,12 @@
+package movie;
+
+import javax.swing.JOptionPane; 
+import movie.ui.UIPeliculas;
+
+public class Main {
+
+    public static void main(String[] args) {
+
+        UIPeliculas.interfazUsuario();                        
+    }    
+}
